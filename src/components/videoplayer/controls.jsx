ad9d750bc0b0a0a034f679ac0050a699551b9d27
lee.paulison.jr <lee.paulison.jr@gmail.com>
2024-03-15 20:06:56 +0000

    Video updated - Player functioning

--- src/components/videoplayer/controls.jsx
+++ src/components/videoplayer/controls.jsx
@@ -1,7 +1,28 @@
-export const Controls = () => {
+import React from "react";
+import PropTypes from "prop-types";
+import { SeekBar } from "./seekbar";
+import { Volume } from "./volume";
+
+export const Controls = (props) => {
+  const { videoPlayer, state } = props;
+  const [sync, setSync] = React.useState(0);
+
+  console.log("State: ", state);
+
+  const controlsState = Object.assign(state, { setSync });
+
   return (
     <div>
-      <h1>Controls</h1>
+      <SeekBar videoPlayer={videoPlayer} state={controlsState} />
+      <div className='flex flex-row justify-between'>
+        <h1>Controls</h1>
+        <Volume videoPlayer={videoPlayer} />
+      </div>
     </div>
   );
+};
+
+Controls.propTypes = {
+  videoPlayer: PropTypes.object,
+  state: PropTypes.object,
 };
