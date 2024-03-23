e70d4e8e6c3af1549ff9cbb03229d513db053524
L0ngshanks <lee.paulison.jr@gmail.com>
2024-03-23 15:26:53 -0400

    Rebase main due to LF video

--- src/components/videoplayer/controls.jsx
+++ src/components/videoplayer/controls.jsx
@@ -1,7 +1,51 @@
-export const Controls = () => {
+import React, { useRef, useState } from "react";
+import PropTypes from "prop-types";
+import { SeekBar } from "./seekbar";
+import { Volume } from "./volume";
+
+export const Controls = (props) => {
+  const { videoPlayer, state } = props;
+  const [sync, setSync] = React.useState(0);
+  const seekbar = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  console.log("State: ", state);
+  console.log("Seekbar:", seekbar);
+
+  const controlsState = Object.assign(state, { setSync });
+
+  const PlayPause = () => {
+    if (!isPlaying) {
+      seekbar.current.whilePlaying();
+      videoPlayer.current.play();
+      setIsPlaying(true);
+    }
+
+    if (isPlaying) {
+      seekbar.current.notPlaying();
+      videoPlayer.current.pause();
+      setIsPlaying(false);
+    }
+  };
+
   return (
     <div>
-      <h1>Controls</h1>
+      <SeekBar videoPlayer={videoPlayer} state={controlsState} ref={seekbar} />
+      <div className='flex flex-row justify-between'>
+        <i onClick={() => PlayPause()}>
+          {isPlaying ? (
+            <span className='material-symbols-outlined'>pause</span>
+          ) : (
+            <span className='material-symbols-outlined'>play_arrow</span>
+          )}
+        </i>
+        <Volume videoPlayer={videoPlayer} />
+      </div>
     </div>
   );
+};
+
+Controls.propTypes = {
+  videoPlayer: PropTypes.object,
+  state: PropTypes.object,
 };
