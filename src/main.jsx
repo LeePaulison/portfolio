23bae7562410544dced0c4577087bccc73f5dccd
L0ngshanks <lee.paulison.jr@gmail.com>
2024-05-07 19:41:11 -0400

    work

--- src/main.jsx
+++ src/main.jsx
@@ -9,14 +9,6 @@ import { Root } from "./root";
 import { ErrorPage } from "./errorpage";
 import { About } from "./pages/about";
 import { Contact } from "./pages/contact";
-import { ToDo } from "./pages/projects/todo";
-import { Create } from "./components/todo/create";
-import { Edit } from "./components/todo/edit";
-import { ToDoItem } from "./components/todo/todoItem";
-import { VideoPlayer } from "./pages/projects/videoplayer";
-
-import { allToDoLoader, todoLoader } from "./loaders/loaders";
-import { createToDoAction, updateToDoAction, deleteToDoAction } from "./actions/actions";
 
 const router = createBrowserRouter([
   {
@@ -39,49 +31,6 @@ const router = createBrowserRouter([
         element: <Contact />,
         handle: {
           crumbs: () => "Contact",
-        },
-      },
-      {
-        path: "/todolist",
-        element: <ToDo />,
-        handle: {
-          crumbs: () => "To Do List",
-        },
-        loader: allToDoLoader,
-        children: [
-          {
-            path: "/todolist/create",
-            element: <Create />,
-            handle: {
-              crumbs: () => "Create ToDo",
-            },
-            action: createToDoAction,
-          },
-          {
-            path: "/todolist/:todoId/edit",
-            element: <Edit />,
-            handle: {
-              crumbs: () => "Edit ToDo",
-            },
-            action: updateToDoAction,
-            loader: todoLoader,
-          },
-          {
-            path: "/todolist/:todoId",
-            element: <ToDoItem />,
-            handle: {
-              crumbs: () => `ToDo Item`,
-            },
-            loader: todoLoader,
-          },
-          { path: "/todolist/:todoId/destroy", action: deleteToDoAction },
-        ],
-      },
-      {
-        path: "/videoplayer",
-        element: <VideoPlayer />,
-        handle: {
-          crumbs: () => "Video Player",
         },
       },
     ],
