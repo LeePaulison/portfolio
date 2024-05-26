6fc8e7eccdaa4cb1fd59b027468e74ba6a541563
L0ngshanks <lee.paulison.jr@gmail.com>
2024-05-25 22:18:31 -0400

    Server console.log

--- functions/index.js
+++ functions/index.js
@@ -10,18 +10,23 @@ admin.initializeApp();
 exports.verifyRecaptcha = functions.https.onRequest(async (req, res) => {
   cors(req, res, async () => {
     try {
+      // pull reCAPTCHA token from request body
       const recaptchaResponse = req.body.recaptcha;
-      const secretKey = process.env.RECAPTCHA_SECRET_KEY; // The secret key from reCAPTCHA
 
+      // reCAPTCHA Secret Key
+      const secretKey = process.env.RECAPTCHA_SECRET_KEY;
+
+      // reCaptcha Token gateway
       if (!recaptchaResponse) {
         return res.status(400).send("reCAPTCHA response missing");
       }
 
+      // reCaptcha Secret Key gateway
       if (!secretKey) {
-        console.error("reCAPTCHA secret key is missing");
         return res.status(500).send("Server configuration error");
       }
 
+      // Verify reCAPTCHA token
       const response = await fetch(`https://www.google.com/recaptcha/api/siteverify`, {
         method: "POST",
         headers: {
@@ -30,13 +35,15 @@ exports.verifyRecaptcha = functions.https.onRequest(async (req, res) => {
         body: `secret=${secretKey}&response=${recaptchaResponse}`,
       });
 
-      const data = await response.json(); // Directly parse response as JSON
+      // Parse response from reCAPTCHA
+      const data = await response.json();
 
+      // Return response from reCAPTCHA to client
       if (data) {
         res.send(data);
       }
     } catch (error) {
-      console.error("Error verifying reCAPTCHA:", error);
+      // Handle error
       return res.status(500).send("Error verifying reCAPTCHA");
     }
   });
