f0ae05887280eec790af6c4cdb95cbe6966c8fd9
L0ngshanks <lee.paulison.jr@gmail.com>
2024-05-26 17:31:33 -0400

    Contact Me needs more aria-role work

--- src/pages/contact/components/confirmation.jsx
+++ src/pages/contact/components/confirmation.jsx
@@ -7,18 +7,26 @@ export const Confirmation = forwardRef(function Confirmation(props, ref) {
   };
 
   return (
-    <dialog ref={ref} className='w-full lg:w-[50%] rounded-md'>
+    <dialog
+      ref={ref}
+      className='w-full lg:w-[50%] rounded-md'
+      role='dialog'
+      aria-labelledby='dialog-title'
+      aria-describedby='dialog-desc'
+    >
       <header className='flex flex-row bg-stone-200 p-2'>
         <h2 className='my-auto ms-2'>Confirmation</h2>
-        <button id='close-button' onClick={() => handleClose()}>
+        <button id='close-button' onClick={() => handleClose()} aria-label='Close Button'>
           <svg xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 -960 960 960' width='24px' fill='#e8eaed'>
             <path d='m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z' />
           </svg>
         </button>
       </header>
       <div className='p-4'>
-        <h1 className='mb-2'>Thank you for your message!</h1>
-        <p>We will get back to you as soon as possible.</p>
+        <h1 className='mb-2' id='dialog-title'>
+          Thank you for your message!
+        </h1>
+        <p id='dialog-desc'>We will get back to you as soon as possible.</p>
       </div>
     </dialog>
   );
--- src/pages/contact/contact_testing.jsx
+++ src/pages/contact/contact_testing.jsx
@@ -22,52 +22,21 @@ export function Contact_Testing() {
   });
   const [isFormValid, setIsFormValid] = useState(false);
   const grecaptcha = window.grecaptcha;
-  const [showConfirmation, setShowConfirmation] = useState(false);
   const dialog = useRef(null);
-
-  const initializeInputValidation = useCallback(() => {
-    const nameInput = document.getElementById("name");
-    const messageInput = document.getElementById("message");
-
-    if (nameInput) {
-      nameInput.addEventListener("input", validateMessage);
-    }
-    if (messageInput) {
-      messageInput.addEventListener("input", validateMessage);
-    }
-  }, []);
-
-  const removeInputValidation = useCallback(() => {
-    const nameInput = document.getElementById("name");
-    const messageInput = document.getElementById("message");
-
-    if (nameInput) {
-      nameInput.removeEventListener("input", validateMessage);
-    }
-    if (messageInput) {
-      messageInput.removeEventListener("input", validateMessage);
-    }
-  }, []);
-
-  useEffect(() => {
-    initializeInputValidation();
-
-    return () => {
-      removeInputValidation();
-    };
-  }, [initializeInputValidation, removeInputValidation]);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
+  const resetFormData = () => {
+    setFormData({
+      from_name: "",
+      from_company: "",
+      from_email: "",
+      message: "",
+    });
   };
 
-  const validateMessage = () => {
+  const validateMessage = useCallback(() => {
     const nameField = document.getElementById("name");
     const messageField = document.getElementById("message");
+
+    clearErrorAlert();
 
     const constraint = new RegExp(/<(.|\n)*?>/g, "");
 
@@ -85,6 +54,54 @@ export function Contact_Testing() {
     }
 
     return setIsFormValid(true);
+  }, []);
+
+  const initializeInputValidation = useCallback(() => {
+    const nameInput = document.getElementById("name");
+    const messageInput = document.getElementById("message");
+
+    if (nameInput) {
+      nameInput.addEventListener("input", validateMessage);
+    }
+    if (messageInput) {
+      messageInput.addEventListener("input", validateMessage);
+    }
+  }, [validateMessage]);
+
+  const removeInputValidation = useCallback(() => {
+    const nameInput = document.getElementById("name");
+    const messageInput = document.getElementById("message");
+
+    if (nameInput) {
+      nameInput.removeEventListener("input", validateMessage);
+    }
+    if (messageInput) {
+      messageInput.removeEventListener("input", validateMessage);
+    }
+  }, [validateMessage]);
+
+  useEffect(() => {
+    initializeInputValidation();
+
+    return () => {
+      removeInputValidation();
+    };
+  }, [initializeInputValidation, removeInputValidation]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  // Remove role attribute from error messages
+  const clearErrorAlert = () => {
+    const errorMessages = document.querySelectorAll("[role='alert']");
+    errorMessages.forEach((message) => {
+      message.removeAttribute("role");
+    });
   };
 
   const sendEmail = (data) => {
@@ -124,6 +141,7 @@ export function Contact_Testing() {
       // Testing URL
       // const recaptchaURL = import.meta.env.VITE_APP_RECAPTCHA_TESTING_URL;
 
+      // Send reCAPTCHA token to the server
       const response = await fetch(recaptchaURL, {
         method: "POST",
         headers: {
@@ -132,57 +150,75 @@ export function Contact_Testing() {
         body: JSON.stringify({ recaptcha: recaptchaResponse }),
       });
 
+      // Parse the response from the server
       const data = await response.json();
 
+      // Check if the server response is successful
+      // If successful, process the form data or send email
+      // Response also contains the reCAPTCHA score
+      // Score may provide additional information for further processing
       if (data.success) {
-        // Process form data or send email
         sendEmail(formData); // Send email
         // Reset form fields
-        setFormData({ from_name: "", from_company: "", from_email: "", message: "" });
-        setShowConfirmation(true);
+        resetFormData();
         return true;
       } else {
         // Reset form fields
-        setFormData({ from_name: "", from_company: "", from_email: "", message: "" });
-        setShowConfirmation(true);
+        resetFormData();
         return false;
       }
     } catch (error) {
+      // Any error in the client-side validation
       console.error("Client Error verifying reCAPTCHA", error);
       return false;
     }
   };
 
+  // Show confirmation dialog
+  // Dialog is shown regardless of the reCAPTCHA validation result
+  // This may reduce the chance of reCAPTCHA token abuse
   const showConfirmationDialog = () => {
     if (dialog) {
       dialog.current.showModal();
     }
   };
 
+  // Handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    // Validate form fields
     validateMessage();
     if (!isFormValid) return;
+
+    // Handle reCAPTCHA
     handleReCaptcha();
+
+    // Show confirmation dialog
     showConfirmationDialog();
   };
-
-  console.log("Show Confirmation: ", showConfirmation);
 
   return (
     <div className='flex place-items-center w-full h-full'>
       <div className='flex flex-col place-items-center w-full lg:max-w-[50%] mx-auto'>
         <h1 className='text-3xl font-bold text-center'>Contact Me</h1>
-        <form onSubmit={handleSubmit} className='w-full'>
+        <form onSubmit={handleSubmit} className='w-full' aria-live='polite'>
           <div className='flex flex-col'>
             <label htmlFor='name'>
               Name<span style={sup}>*</span>:
             </label>
-            <input type='text' id='name' name='from_name' value={formData.from_name} onChange={handleChange} required />
+            <input
+              type='text'
+              id='name'
+              name='from_name'
+              value={formData.from_name}
+              onChange={handleChange}
+              required
+              aria-required='true'
+            />
           </div>
           <div className='flex flex-col'>
-            <label htmlFor='name'>Company:</label>
+            <label htmlFor='company'>Company:</label>
             <input type='text' id='company' name='from_company' value={formData.from_company} onChange={handleChange} />
           </div>
 
@@ -197,6 +233,7 @@ export function Contact_Testing() {
               value={formData.from_email}
               onChange={handleChange}
               required
+              aria-required='true'
             />
           </div>
           <div className='flex flex-col'>
@@ -214,7 +251,7 @@ export function Contact_Testing() {
             <button type='submit' className='btn-submit'>
               Submit
             </button>
-            <button type='reset' className='btn' onClick={() => setFormData({ name: "", email: "", message: "" })}>
+            <button type='reset' className='btn' onClick={() => resetFormData()}>
               Reset
             </button>
             <button
